8579be17d56df495f3fc5f730ae15efd1c1acafa
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-03-09 15:23:48 +0900

    テーブル情報を元に接続するようにしました

--- API/main.py
+++ API/main.py
@@ -3,14 +3,15 @@ from typing import Union
 from fastapi import FastAPI
 
 #MySQLに接続するスクリプト
-from Extend import database
+from Extend import table
 
 app = FastAPI()
+
+Wroktime = table.Worktime
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
